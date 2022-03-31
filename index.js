c486bceda219f0e8b0ab7784999ce68d11360c1c
Gabriel Carino <gabejcarino@gmail.com>
2022-03-30 21:28:47 -0500

    commit tree 14f41d3e85cc5826dcab2bf4896b3bd7a59ac2aa

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+const drivers = ["Bill", "Mike", "John", "Gabe", "Sean", "Dave"];
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0,2);
+}
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+const createFareMultiplier =  function(fareMultiplier) {
+    return function(value) {
+        return fareMultiplier * value;
+    }
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+function selectDifferentDrivers(drivers, func) {
+    return func(drivers);
+}
